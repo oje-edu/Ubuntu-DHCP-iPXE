30436840336c99cf8c57f8a685106ed9df3c1e6f
psychopomposwow <Thorsten.Oje@t-online.de>
2020-06-26 19:58:57 +0200

    Typo

--- scripts/ipxe.sh
+++ scripts/ipxe.sh
@@ -2,8 +2,7 @@
 
 sudo apt install git build-essential liblzma-dev zlib1g-dev binutils-dev genisoimage uuid-dev -y
 rm -rf /tmp/ipxe
-mkdir -p /tmp/ipxe
-cd /tmp/ipxe
+cd /tmp
 git clone git://git.ipxe.org/ipxe.git ipxe
 cd /tmp/ipxe/src
 make && make bin/NIC
