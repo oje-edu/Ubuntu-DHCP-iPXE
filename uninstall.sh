d6800cc2c52506716f85453953969fefdbbd1837
oje-edu <thorsten.oje@gfn.education>
2021-04-08 20:20:51 +0200

    added uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -2,12 +2,12 @@
 
 
 # Disable DHCP-Server
-sudo systemctl disable--now isc-dhcp-server
+sudo systemctl disable --now isc-dhcp-server
 
 # Disable TFTP Server
 sudo systemctl disable --now tftpd-hpa
 
 # Remove DHCP and TFTP and empty the tftpboot folder
-sudo apt remove isc-dhcp-server tftpd-hpa -y && sudo apt autoremove
+sudo apt remove isc-dhcp-server tftpd-hpa -y && sudo apt autoremove -y
 sudo rm -rf /tftpboot/*
 
