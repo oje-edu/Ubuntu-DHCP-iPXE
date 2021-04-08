960d81cbb10b9fc356229ce4efcf170163ba0fc7
oje-edu <thorsten.oje@gfn.education>
2021-04-08 20:19:15 +0200

    added uninstall

---
+++ uninstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+# Disable DHCP-Server
+sudo systemctl disable--now isc-dhcp-server
+
+# Disable TFTP Server
+sudo systemctl disable --now tftpd-hpa
+
+# Remove DHCP and TFTP and empty the tftpboot folder
+sudo apt remove isc-dhcp-server tftpd-hpa -y && sudo apt autoremove
+sudo rm -rf /tftpboot/*
+
